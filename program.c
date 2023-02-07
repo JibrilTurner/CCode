4af4cce19ad94e28afaeda9b21c06d63bc55a240
JibrilTurner <121262046+JibrilTurner@users.noreply.github.com>
2023-02-06 17:38:37 -0800

    Update program.c

--- program.c
+++ program.c
@@ -5,15 +5,11 @@ int programopenandclose(char userexit_test[])
 {
     char exitcondition[50] = "0";  // declaration of char array
     int value; int a;
-
     do
     {
-
         printf("enter 0 to exit\nenter 1 to loop");
         scanf("%s", userexit_test);
-
         value=strcmp(exitcondition , userexit_test);//when comparing strings
-
         if(value==0)
         {
             printf("\nExiting\n");
@@ -27,12 +23,8 @@ int programopenandclose(char userexit_test[])
     }
     while(value != 0);
     return a ;
-
 }
-
-
 int main() {
-
     char userexit_test[30];
     int ansa = programopenandclose(userexit_test);
     return 0;
